2acd8ace8841c6e0ff366424255141369269225a
vgabrielk <vfelix1553@gmail.com>
2022-02-14 02:44:29 -0300

    memo

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Share from '../Share/Share';
+
 import './Info.css'
+import InfoMap from './InfoMap';
 
 
 const Info = ({ news }) => {
@@ -15,32 +16,7 @@ const Info = ({ news }) => {
       </div>
 
 
-      {news.filter((val) => {
-        if (input === '') {
-          return val;
-        }
-        return val.state.toLowerCase().includes(input.toLowerCase()) || val.uf.toLowerCase().includes(input.toLowerCase())
-
-      }).map(index => (
-        <div className="statistic" data-aos="fade">
-          <Share />
-          <div className="statistic-title">
-            <h2>{index.state}</h2>
-          </div>
-          <div className="statistic-content">
-            <div className="statistic-cases">
-              <span> Número de casos: <br /> <b>{index.cases}</b> </span> <br />
-            </div>
-            <div className="statistic-deaths">
-              <span>Óbitos : <br /> <b>{index.deaths}</b> </span> <br />
-            </div>
-            <div className="statistic-suspects">
-              <span>Suspeitas : <br /> <b>{index.suspects}</b> </span>
-            </div>
-          </div>
-        </div>
-      ))}
-
+    <InfoMap news ={news} input={input}/>
     </>
   );
 }
---
+++ src/components/Info/InfoMap.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Share from '../Share/Share';
+import { memo } from 'react'
+const InfoMap = ({news, input}) => {
+    return ( 
+        <>
+          {news.filter((val) => {
+        if (input === '') {
+          return val;
+        }
+        return val.state.toLowerCase().includes(input.toLowerCase()) || val.uf.toLowerCase().includes(input.toLowerCase())
+
+      }).map(index => (
+        <div className="statistic" data-aos="fade">
+          <Share />
+          <div className="statistic-title">
+            <h2>{index.state}</h2>
+          </div>
+          <div className="statistic-content">
+            <div className="statistic-cases">
+              <span> Número de casos: <br /> <b>{index.cases}</b> </span> <br />
+            </div>
+            <div className="statistic-deaths">
+              <span>Óbitos : <br /> <b>{index.deaths}</b> </span> <br />
+            </div>
+            <div className="statistic-suspects">
+              <span>Suspeitas : <br /> <b>{index.suspects}</b> </span>
+            </div>
+          </div>
+        </div>
+      ))}
+
+        </>
+     );
+}
+ 
+export default memo(InfoMap);
